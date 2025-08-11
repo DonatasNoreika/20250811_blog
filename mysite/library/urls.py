408ec1d97d9c51af818664fceca17b180f8fa8e3
DonatasNoreika <donoras@gmail.com>
2025-08-11 19:51:22 +0300

    Pridėta knygų paieška pagal pavadinimą, aprašymą, autoriaus vardą arba pavardę

--- mysite/library/urls.py
+++ mysite/library/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path("", views.books, name="books"),
     path("books/<int:book_id>", views.book, name="book"),
+    path("search/", views.search, name='search'),
 ]
--- mysite/library/views.py
+++ mysite/library/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Book, Genre, Author
 from django.core.paginator import Paginator
+from django.db.models import Q
+
 
 def books(request):
     objects = Book.objects.all()
@@ -9,6 +11,18 @@ def books(request):
     books = paginator.get_page(page_num)
     return render(request, template_name="books.html", context={"books": books})
 
+
 def book(request, book_id):
     book = Book.objects.get(pk=book_id)
     return render(request, template_name="book.html", context={"book": book})
+
+
+def search(request):
+    query = request.GET.get("q")
+    context = {
+        "books": Book.objects.filter(
+            Q(title__icontains=query) | Q(summary__icontains=query) | Q(author__first_name__icontains=query)
+            | Q(author__last_name__icontains=query)),
+        "query": query,
+    }
+    return render(request, template_name="search.html", context=context)
