580978defdb78be56013f59dc4490f0aec7d07cf
DonatasNoreika <donoras@gmail.com>
2025-08-11 18:59:31 +0300

    Pridėti knygų list ir detail view'sai

--- mysite/library/urls.py
+++ mysite/library/urls.py
@@ -1,7 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-
+    path("", views.books, name="books"),
+    path("books/<int:book_id>", views.book, name="book"),
 ]
--- mysite/library/views.py
+++ mysite/library/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Book, Genre, Author
 
-# Create your views here.
+def books(request):
+    books = Book.objects.all()
+    return render(request, template_name="books.html", context={"books": books})
+
+def book(request, book_id):
+    book = Book.objects.get(pk=book_id)
+    return render(request, template_name="book.html", context={"book": book})
